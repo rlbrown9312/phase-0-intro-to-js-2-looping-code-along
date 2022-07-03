726426a66eac258927a9120e612897344e33c363
Richard <richard.brown0807@gmail.com>
2022-07-03 18:21:11 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+ //const names = ["Guadalupe", "Ollie", "Aki"];
+ //const event = "Christmas"; 
+
+ function writeCards(names, event) {
+     let messages = [] 
+     for (let i = 0; i < names.length; i ++) {
+        let newMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        messages.push(newMessage)   
+     }
+
+     return messages
+ }
+
+
+// // function countDown(startingNumber) {
+// //     while (startingNumber >= 0); {
+// //         console.log(startingNumber)
+// //         startingNumber--;
+// //     }
+// //     console.log(startingNumber)
+         
+// }

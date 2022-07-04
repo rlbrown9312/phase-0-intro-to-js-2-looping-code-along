35d566bf6b878bd86774c0acccc6b2cde240a480
Richard <richard.brown0807@gmail.com>
2022-07-04 16:01:39 -0500

    Done with Assignment

--- index.js
+++ index.js
@@ -12,11 +12,11 @@
  }
 
 
-// // function countDown(startingNumber) {
-// //     while (startingNumber >= 0); {
-// //         console.log(startingNumber)
-// //         startingNumber--;
-// //     }
-// //     console.log(startingNumber)
+function countDown(startingNumber) {
+    while (startingNumber >= 0); {
+        console.log(startingNumber)
+        startingNumber--;
+    }
+    console.log(startingNumber)
          
-// }
+}

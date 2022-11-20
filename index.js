901d2df116f5290b5e0ce71a67b41af3f6397305
Richard <richard.brown0807@gmail.com>
2022-11-19 21:06:41 -0600

    commit tree 0b40f87556ec86f93fa6fcb1349eed7f3677eb9c

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
- //const names = ["Guadalupe", "Ollie", "Aki"];
- //const event = "Christmas"; 
+const names = ["Guadalupe", "Ollie", "Aki"];
+  //const event = "Christmas"; 
 
- function writeCards(names, event) {
-     let messages = [] 
-     for (let i = 0; i < names.length; i ++) {
-        let newMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
-        messages.push(newMessage)   
-     }
+  const writeCards = (arr) => {
+    let array = []
 
-     return messages
- }
-
- 
- 
- 
- function countDown(startingNumber) {
-    while (startingNumber >= 0) {
-      console.log(startingNumber)
-        startingNumber--;
+    for (let i = 0; i < arr.length; i++) {
+        const message = `Thank you ${arr[i]}, for the wonderful ${surprise} gift!`
+        array.push(message)
+        // console.log(arr[i])
     }
+        return array
 }
-countDown(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  function writeCards(names, event) {
+//      let messages = [] 
+//      for (let i = 0; i < names.length; i ++) {
+//         let newMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+//         messages.push(newMessage)   
+//      }
+
+//      return messages
+//  }
+
+ 
+ 
+ 
+//  function countDown(startingNumber) {
+//     while (startingNumber >= 0) {
+//       console.log(startingNumber)
+//         startingNumber--;
+//     }
+// }
+// countDown(10)
  
  
 
